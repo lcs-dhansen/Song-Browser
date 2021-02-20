f6f563ae9cc24d348933596030187c01c2d49f4d
Devon Hanses <dhansen@lcs.on.ca>
2021-02-20 09:49:35 -0500

    Initial Commit

---
+++ Song Browser/Song_BrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Song_BrowserApp.swift
+//  Song Browser
+//
+//  Created by Devon Hansen on 2021-02-20.
+//
+
+import SwiftUI
+
+@main
+struct Song_BrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
